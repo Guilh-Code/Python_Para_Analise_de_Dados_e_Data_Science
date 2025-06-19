237ec583fee25f43e7666425d0324ec8eaee2056
Guilh-Code <GuilhermeRAR2005@gmail.com>
2025-06-19 17:51:39 -0300

    Iniciando projeto POO Fundação Bradesco

---
+++ Cap8 POO/Curso_Bradesco_POO/Projeto Controle Bancário/Main.py
@@ -0,0 +1,12 @@
+class Main:
+    pass
+
+print("Testando o projeto")
+
+from Cliente import Cliente
+from Conta import Conta
+
+c1 = Cliente("João", "114444-2222")
+conta = Conta(c1.nome, 6565, 0)
+
+print(f"{conta.titular} Número: {conta.numero} Seu saldo: {conta.saldo}")
