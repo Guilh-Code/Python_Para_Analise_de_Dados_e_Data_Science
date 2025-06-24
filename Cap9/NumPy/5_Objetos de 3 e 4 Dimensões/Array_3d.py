768c5d26bb8fe711bdc766d4c255c757760626c7
Guilh-Code <GuilhermeRAR2005@gmail.com>
2025-06-24 18:59:01 -0300

    Manipulando Objetos de 3 e 4 Dimensões em NumPy

---
+++ Cap9/NumPy/5_Objetos de 3 e 4 Dimensões/Array_3d.py
@@ -0,0 +1,22 @@
+import numpy as dsa
+
+# Cria um array numpy de 3 dimensões
+arr_3d = dsa.array([
+    [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+    ],
+    [
+        [13, 14, 15, 16],
+        [17, 18, 19, 20],
+        [21, 22, 23, 24]
+    ]
+])
+
+print(arr_3d)
+print()
+
+print(arr_3d.ndim)
+print(arr_3d.shape)
+print(arr_3d[0, 2, 1])
